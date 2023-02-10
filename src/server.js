a86820c7acec7b6040eb239418b7e979fc17a070
Vitor Mariotto <vitormariotto03@gmail.com>
2023-02-10 17:18:15 -0300

    require json e reading json

--- src/server.js
+++ src/server.js
@@ -1,14 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3200
+const express = require('express');
+const listaCurso = require('./bdd/cursos.json');//importar arquivo json
+const app = express();
+const host = 'localhost';
+const port = 3200;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    const msg = [{nome: "LP2"}, {nome: "PJ3"}];
+  res.json(msg);
+})
+
+app.get('/cursos', (req, res) => {
+    res.json(listaCurso);//ler arquivo json
 })
 app.get('/cursos', (req, res) => {
-  res.send('Hello curse!')
+  res.send('Hello GET curso!')
+})
+
+app.post('/cursos', (req, res) => {
+  res.send('Hello POST curse!')
+})
+
+app.delete('/cursos', (req, res) => {
+  res.send('Hello delete curse!')
+})
+
+app.all('*', (req, res) => {
+  res.send('Não encontrada')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://${host}:${port}`)
 })
