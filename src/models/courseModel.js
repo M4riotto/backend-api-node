6ecc2d16179f3c78c57ff0148ed80d426e521c4d
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-02-17 22:39:20 -0300

    comentarios

--- src/models/courseModel.js
+++ src/models/courseModel.js
@@ -16,21 +16,17 @@ courseModel.listAllCourses = (callback) => {
       callback(null, result)
     }
   })
-}
+}// A função listAllCourses se conecta a um banco de dados MySQL local e executa uma consulta para selecionar todos os cursos na tabela cursos.Se houver um erro, a função chama a função de retorno de chamada(callback) passando o erro como primeiro argumento e nulo como segundo argumento.Se não houver erro, a função chama a função de retorno de chamada passando nulo como primeiro argumento e o resultado da consulta como segundo argumento.
 
 courseModel.createCourse = (req, res) => {
   res.json({ message: "Entrou na rota /course com POST!" })
-}
+}// A função createCourse não está implementada e simplesmente retorna um objeto JSON com uma mensagem.
 
 module.exports = courseModel
 
+// O comando module.exports = courseModel exporta o objeto courseModel para que possa ser usado em outros arquivos como um módulo.
+
 
 // Esse código define um objeto chamado courseModel, que contém duas funções: listAllCourses e createCourse.
-
-// A função listAllCourses se conecta a um banco de dados MySQL local e executa uma consulta para selecionar todos os cursos na tabela cursos.Se houver um erro, a função chama a função de retorno de chamada(callback) passando o erro como primeiro argumento e nulo como segundo argumento.Se não houver erro, a função chama a função de retorno de chamada passando nulo como primeiro argumento e o resultado da consulta como segundo argumento.
-
-// A função createCourse não está implementada e simplesmente retorna um objeto JSON com uma mensagem.
-
-// O comando module.exports = courseModel exporta o objeto courseModel para que possa ser usado em outros arquivos como um módulo.
 
 // Em resumo, este código define duas funções que podem ser usadas para listar todos os cursos e criar um novo curso no banco de dados MySQL local.
--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -1,21 +1,10 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express');// O módulo express é importado para criar o objeto router.
+const router = express.Router();// Este código define um objeto router que é um objeto de roteamento do Express.O roteador permite definir rotas para uma aplicação Express e associar funções de controlador a essas rotas.
 
-const courseController = require('../controllers/courseController')
+const courseController = require('../controllers/courseController')// O módulo courseController é importado para que possa ser usado para associar as funções de controlador às rotas.
 
-router.get('/', courseController.listAllCourses);
-router.post('/', courseController.createCourse);
+router.get('/', courseController.listAllCourses);// A primeira rota associada a este roteador é um rota GET para o caminho raiz '/'.A função listAllCourses do courseController é associada a esta rota, o que significa que, quando um cliente fizer uma solicitação GET para a rota '/', a função listAllCourses será chamada para manipular essa solicitação.
+router.post('/', courseController.createCourse);// A segunda rota associada a este roteador é uma rota POST para o caminho raiz '/'.A função createCourse do courseController é associada a esta rota, o que significa que, quando um cliente fizer uma solicitação POST para a rota '/', a função createCourse será chamada para manipular essa solicitação.
 
 module.exports = router;
-
-// Este código define um objeto router que é um objeto de roteamento do Express.O roteador permite definir rotas para uma aplicação Express e associar funções de controlador a essas rotas.
-
-// O módulo express é importado para criar o objeto router.
-
-// O módulo courseController é importado para que possa ser usado para associar as funções de controlador às rotas.
-
-// A primeira rota associada a este roteador é um rota GET para o caminho raiz '/'.A função listAllCourses do courseController é associada a esta rota, o que significa que, quando um cliente fizer uma solicitação GET para a rota '/', a função listAllCourses será chamada para manipular essa solicitação.
-
-// A segunda rota associada a este roteador é uma rota POST para o caminho raiz '/'.A função createCourse do courseController é associada a esta rota, o que significa que, quando um cliente fizer uma solicitação POST para a rota '/', a função createCourse será chamada para manipular essa solicitação.
-
 // O comando module.exports = router exporta o objeto router para que possa ser usado em outros arquivos como um módulo.Isso permite que outros arquivos acessem as rotas definidas neste roteador e usem as funções de controlador associadas a essas rotas.
